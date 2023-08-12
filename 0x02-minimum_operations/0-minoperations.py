e7a53161d70e5cfa6bc16f2f702601a306c601bc
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-08-12 20:33:33 +0100

    chore: reformatted

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 """Minimum Operations"""
+
+
 def minOperations(n):
     """Calculates the fewest number of operations needed to result in exactly n H characters in the file."""
     if n <= 1:
