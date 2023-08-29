25a13137457413b17ed4ebec0976140340e61531
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-08-29 12:25:31 +0100

    chore: all commits

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import sys
+
+def is_safe(board, row, col, N):
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+    
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 1:
+            return False
+    
+    return True
+
+def solve_nqueens(board, row, N):
+    if row == N:
+        # Print the solution in the desired format
+        solution = []
+        for i in range(N):
+            solution.append([i, board[i].index(1)])
+        print(solution)
+        return
+    
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row][col] = 1
+            solve_nqueens(board, row + 1, N)
+            board[row][col] = 0
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    
+    board = [[0] * N for _ in range(N)]
+    solve_nqueens(board, 0, N)
+
+if __name__ == "__main__":
+    main()
---
+++ 0x05-nqueens/README.md
@@ -0,0 +1,5 @@
+# N Queens Problem Solver
+
+## Description
+
+This program is designed to solve the N Queens Problem, which involves placing N non-attacking queens on an N×N chessboard. The program generates and prints all possible solutions to the problem for a given N.
