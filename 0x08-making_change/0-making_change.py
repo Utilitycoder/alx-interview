f083101699e9b5841aa16c81119e2ae79a767ff9
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-09-29 12:19:27 +0100

    Chore: made chnages

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,43 @@
 #!/usr/bin/python3
+"""
+Making Change
+"""
+
 
 def makeChange(coins, total):
-    if total < 0:
-        return -1  # Total cannot be met with the given coins
+    """
+    Return the minimum number of coins needed to meet a given total
+    Args:
+        coins (list of ints): a list of coins of different values
+        total (int): total value to be met
+    Return:
+        Number of coins or -1 if meeting the total is not possible
+    """
+    if total <= 0:
+        return 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
 
-    # Initialize a list to store the minimum number of coins for each total from 0 to 'total'
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins needed to make a total of 0
-
-    for coin in coins:
-        for i in range(coin, total + 1):
-            # Update dp[i] if using 'coin' leads to a smaller number of coins needed
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
-
-# Example usage:
-coins = [1, 2, 5]
-total = 11
-result = makeChange(coins, total)
-print(result)  # Output should be 3 (using two 5-coins and one 1-coin)
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
